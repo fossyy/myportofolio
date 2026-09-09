8e05f75e7b7fc024a4bc368a744510c6ab5eabfa
Bagas <bagas@fossy.my.id>
2026-09-09 20:35:25 +0700

    feat: initial tutorial step

---
+++ main/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Burhan",
+        "npm": "2206000000",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "A Computer Science student at Universitas Indonesia interested "
+            "in software development and education."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Burhan",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
